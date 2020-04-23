fa1ddfa8268cddd606a69fde108e7c5a13a2c581
Mattheus Einloft <mattheuseinloft@gmail.com>
2020-04-23 11:45:35 -0300

    repositories list status code

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  return response.json(repositories);
+  return response.status(200).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
